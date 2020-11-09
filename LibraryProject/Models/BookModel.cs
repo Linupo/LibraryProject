03b1987c4151e4b966d8055e9c902fd432084d51
Gustas Klevinskas <programotuojes@users.noreply.github.com>
2020-11-09 20:40:24 +0200

    Add models

---
+++ LibraryProject/Models/BookModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LibraryProject.Models
+{
+    public class BookModel
+    {
+        public AuthorModel Author { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int YearWritten { get; set; }
+        public string ISBN { get; set; }
+    }
+}
