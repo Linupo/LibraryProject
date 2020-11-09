c4a9490c272ec17f82dcb02bb2a92c293ed71425
Martynas Bielskus <61162271+martis99@users.noreply.github.com>
2020-11-09 22:47:29 +0200

    Add publisher

--- LibraryProject/Controllers/PublisherController.cs
+++ LibraryProject/Controllers/PublisherController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LibraryProject.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,6 +9,28 @@ namespace LibraryProject.Controllers
 {
     public class PublisherController : Controller
     {
+        public static List<PublisherModel> publishers = new List<PublisherModel>()
+        {
+            new PublisherModel()
+            {
+                Id = 0,
+                Title = "Pegasas",
+                Address = "Karaliaus Mindaugo pr. 49, 4333 Kaunas"
+            },
+           new PublisherModel()
+            {
+                Id = 1,
+                Title = "Obuolys",
+                Address = "Maironio g. 6-1, LT-44302 Kaunas"
+            },
+            new PublisherModel()
+            {
+                Id = 2,
+                Title = "Vaga",
+                Address = "Gedimino pr. 50, LT-01110 Vilnius"
+            }
+        };
+
         public ActionResult Index()
         {
             return View();
---
+++ LibraryProject/Models/PublisherModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LibraryProject.Models
+{
+    public class PublisherModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Address { get; set; }
+    }
+}
