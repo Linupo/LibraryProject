9c0f19a76f14ab18467e12819a851a3bf04d2100
Gustas Klevinskas <programotuojes@users.noreply.github.com>
2020-11-09 20:40:12 +0200

    Add controllers

---
+++ LibraryProject/Controllers/PublisherController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LibraryProject.Controllers
+{
+    public class PublisherController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
