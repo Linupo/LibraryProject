db83f7de9e73bde92e101938cdf8fce1d31565be
sangi-l <74146853+sangi-l@users.noreply.github.com>
2020-11-13 12:57:41 +0200

    Kompiuteriu posistemes langai

--- LibraryProject/Controllers/ComputerController.cs
+++ LibraryProject/Controllers/ComputerController.cs
@@ -8,7 +8,32 @@ namespace LibraryProject.Controllers
 {
     public class ComputerController : Controller
     {
+        // GET: Index
         public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Reserve
+        public ActionResult Reserve()
+        {
+            return View();
+        }
+
+        // GET: Add
+        public ActionResult Add()
+        {
+            return View();
+        }
+
+        // GET: Edit
+        public ActionResult Edit()
+        {
+            return View();
+        }
+
+        // GET: Delete
+        public ActionResult Delete()
         {
             return View();
         }
---
+++ LibraryProject/Models/ComputerModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace LibraryProject.Models
+{
+    public class ComputerModel
+    {
+        [DisplayName("ID")]
+        [Required]
+        public int kodas { get; set; }
+        [DisplayName("Vaizdo plokštė")]
+        [Required]
+        public string vaizdo_plokste { get; set; }
+        [DisplayName("Procesorius")]
+        [Required]
+        public string procesorius { get; set; }
+        [DisplayName("Monitorius")]
+        [Required]
+        public string monitorius { get; set; }
+    }
+}
