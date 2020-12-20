e8d803646459ebd967a0b123067cddf7b3f620ea
Gustas Klevinskas <programotuojes@users.noreply.github.com>
2020-12-20 21:46:21 +0200

    Create user model with new register view

--- LibraryProject/Controllers/AuthController.cs
+++ LibraryProject/Controllers/AuthController.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using LibraryProject.DataAccess;
+using LibraryProject.Models;
 
 namespace LibraryProject.Controllers
 {
     public class AuthController : Controller
     {
+
+
         public ActionResult Login()
         {
             return View();
@@ -15,6 +19,21 @@ namespace LibraryProject.Controllers
 
         public ActionResult Register()
         {
+            return View();
+        }
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register(UserModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                string sql = @"INSERT INTO Users (FirstName, LastName, PersonalCode, Email, Password, IsActive, Birthday)
+                               VALUES (@FirstName, @LastName, @PersonalCode, @Email, @Password, 1, @Birthday);";
+                SqlDataAccess.SaveData(sql, model);
+                return RedirectToAction("Index", "Home");
+            }
+
             return View();
         }
     }
---
+++ LibraryProject/Models/UserModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace LibraryProject.Models
+{
+    public class UserModel
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Vardas")]
+        [Required(ErrorMessage = "Įveskite savo vardą")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Pavardė")]
+        [Required(ErrorMessage = "Įveskite savo pavardę")]
+        public string LastName { get; set; }
+
+        [Display(Name = "Asmens kodas")]
+        [Required(ErrorMessage = "Įveskite savo asmens kodą")]
+        public string PersonalCode { get; set; }
+
+        [Display(Name = "El. pašto adresas")]
+        [Required(ErrorMessage = "Įveskite savo el. pašto adresą")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Display(Name = "Slaptažodis")]
+        [Required(ErrorMessage = "Įveskite slaptažodį")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        public bool IsActive { get; set; }
+
+        [Display(Name = "Gimimo data")]
+        [Required(ErrorMessage = "Nurodykite savo gimimo datą")]
+        [DataType(DataType.Date)]
+        public DateTime Birthday { get; set; }
+    }
+}
